4345c0815d3e68d4c57720a28f0ce8a2d2bc118e
Chappyo7 <130234109+Chappyo7@users.noreply.github.com>
2023-10-16 15:26:22 -0400

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,17 +1,19 @@
 const slider = document.querySelector("#loss-slider");
-const lossValue = document.querySelector(".amount-of-loss");
-const returnToBreakEvenValue = document.querySelector(".return-to-break-even");
-const yearsToBreakEvenValue = document.querySelector(".years-to-break-even");
+const lossDisplay = document.querySelector(".amount-of-loss");
+const returnDisplay = document.querySelector(".return-to-break-even");
+const yearDisplay = document.querySelector(".years-to-break-even");
 
-const lossPercentages = ["10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", "50%"];
-const returnPercentages = ["11.1%", "17.7%", "25%", "33.3%", "42.9%", "53.9%", "66.7%", "81.8%", "100%"];
+const losses = ["10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", "50%"];
+const returns = ["11.1%", "17.7%", "25%", "33.3%", "42.9%", "53.9%", "66.7%", "81.8%", "100%"];
 const years = ["1.1", "1.7", "2.3", "3.0", "3.7", "4.5", "5.4", "6.3", "7.3"];
 
 slider.addEventListener("input", function() {
-    const index = slider.value;
+    const idx = slider.value;
 
-    lossValue.textContent = lossPercentages[index];
-    returnToBreakEvenValue.textContent = returnPercentages[index];
-    yearsToBreakEvenValue.textContent = years[index] + " years";
+    lossDisplay.textContent = losses[idx];
+    returnDisplay.textContent = returns[idx];
+    yearDisplay.textContent = years[idx] + " years";
+    
+    slider.setAttribute('data-content', losses[idx]);
 });
 
